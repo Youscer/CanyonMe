f79a0055baea4fa29d12eb76d47a2d37faf0b54d
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2021-09-16 23:36:22 +0200

    add firebase configs

---
+++ src/main/webapp/app/environment.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCpdsO1nNuTQs8hVGVmdOBqp3vLqrkA1cI',
+  authDomain: 'canyonme-e86ae.firebaseapp.com',
+  projectId: 'canyonme-e86ae',
+  storageBucket: 'canyonme-e86ae.appspot.com',
+  messagingSenderId: '193549212672',
+  appId: '1:193549212672:web:1958882abba4385711cce5',
+  measurementId: 'G-TGZRWVNEWT',
+};
