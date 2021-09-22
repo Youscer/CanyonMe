b84ec363ce369450f7e7df267ac4d4b09912a87d
Youcef <khelil.youcef1@gmail.com>
2021-09-22 06:22:30 +0200

    Added fake imgs in product detail

--- src/main/webapp/app/product/detail/product-detail.component.ts
+++ src/main/webapp/app/product/detail/product-detail.component.ts
@@ -12,6 +12,17 @@ export class ProductDetailComponent implements OnInit {
   product: IProduct | null = null;
   errmsg: string;
 
+  imgs = [
+    "https://firebasestorage.googleapis.com/v0/b/canyonme-e86ae.appspot.com/o/img%2F1632276054774_2008197_ST.jpg?alt=media&token=7888f9a9-fdd5-4b95-ac28-018dda697f05",
+    "https://firebasestorage.googleapis.com/v0/b/canyonme-e86ae.appspot.com/o/img%2F1632276059348_2008197_ST1.jpg?alt=media&token=35f8e35c-f65c-4285-b01a-58a1934722b6",
+    "https://firebasestorage.googleapis.com/v0/b/canyonme-e86ae.appspot.com/o/img%2F1632276063211_2008197_ST2.jpg?alt=media&token=5fe23678-da30-490e-8e31-3029cbcbb2bf",
+    "https://firebasestorage.googleapis.com/v0/b/canyonme-e86ae.appspot.com/o/img%2F1632276054774_2008197_ST.jpg?alt=media&token=7888f9a9-fdd5-4b95-ac28-018dda697f05",
+    "https://firebasestorage.googleapis.com/v0/b/canyonme-e86ae.appspot.com/o/img%2F1632276059348_2008197_ST1.jpg?alt=media&token=35f8e35c-f65c-4285-b01a-58a1934722b6",
+    "https://firebasestorage.googleapis.com/v0/b/canyonme-e86ae.appspot.com/o/img%2F1632276063211_2008197_ST2.jpg?alt=media&token=5fe23678-da30-490e-8e31-3029cbcbb2bf",
+  ];
+
+  imgMain = this.imgs[0];
+
   constructor(protected activatedRoute: ActivatedRoute, protected productService: ProductService) {
     this.errmsg = '';
   }
@@ -22,6 +33,15 @@ export class ProductDetailComponent implements OnInit {
     this.productService.getProduct(Number(id)).subscribe(
       product => {
         this.product = product;
+/*         alert('load picture' + String(product.id));
+ */        if (product.pictures) {
+/*           alert('picture !' + String(product.pictures.length));
+ */          for (const picture of product.pictures) {
+            if (picture.link) {
+/*               alert(picture.link);
+ */            }
+          }
+        }
       },
       error => {
         this.errmsg = error.statusText;
--- src/main/webapp/app/product/product.model.ts
+++ src/main/webapp/app/product/product.model.ts
@@ -1,3 +1,4 @@
+import { IImage } from './../entities/product/product.model';
 export interface IProduct {
   id: number;
   name: string;
@@ -5,10 +6,19 @@ export interface IProduct {
   description: string;
   unitPrice: number;
   quantity: number;
+  pictures?: IImage[] | null;
 }
 
 export class Product implements IProduct {
-  constructor(public id: number, public name: string, public description: string, public unitPrice: number, public quantity: number, public brand: string) {}
+  constructor(
+    public id: number,
+    public name: string,
+    public description: string,
+    public unitPrice: number,
+    public quantity: number,
+    public brand: string,
+    public pictures?: IImage[] | null
+  ) { }
 }
 
 export function getProductIdentifier(product: IProduct): number | undefined {
