866a2e5d222c210463097730d36a7c1f0f9c1447
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2021-09-22 23:57:03 +0200

    finished productshow component

--- src/main/webapp/app/home/product-show/product-show.module.ts
+++ src/main/webapp/app/home/product-show/product-show.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProductShowComponent } from './product-show.component';
 import { ProductGridModule } from 'app/canyon-shared/product-grid/product-grid.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [CommonModule, NgbModule, ProductGridModule],
+  imports: [CommonModule, NgbModule, ProductGridModule, MatButtonModule, MatIconModule],
   declarations: [ProductShowComponent],
   entryComponents: [ProductShowComponent],
   exports: [ProductShowComponent],
